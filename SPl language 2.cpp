f03e0421a0dcc939bf5af8ff155daa1c0c3a48d2
Jerry Zheng <55902789+jzheng100823@users.noreply.github.com>
2021-12-08 19:29:21 -0500

    Update SPl language 2.cpp

--- SPl language 2.cpp
+++ SPl language 2.cpp
@@ -1,21 +1,21 @@
 // C++ program to find factorial of a given number
-#include <iostream>
+#include <iostream> //these are include files that are needed in the file
 using namespace std;
  
 // function to find factorial of given number
-int factorial(int n)
+int factorial(int n) // this declared the factorial function which passes a integer to the function
 {
-    if (n == 0)
+    if (n == 0) //this is the base case 
         return 1;
-    return n * factorial(n - 1);
+    return n * factorial(n - 1); // this is the equation/function to implement factorial
 }
  
 // Driver code
 int main()
 {
-    cout<<"enter a number to find factorial  of"<<endl;
-    int number;
-    cin>>number;
-    cout << "Factorial of "<< number << " is " << factorial(number) << endl;
+    cout<<"enter a number to find factorial  of"<<endl; // prints to tells the user to input any number they want to find factorial of
+    int number; //declares the integer that will store the user input and the number that is used 
+    cin>>number; // actually gets the number from the user
+    cout << "Factorial of "<< number << " is " << factorial(number) << endl; // prints out the factorial of the nummber
     return 0;
 }
