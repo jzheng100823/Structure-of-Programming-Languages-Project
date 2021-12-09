1a21270b6f8eb99942b4b62dc69d78fbcf8da2c8
Jerry Zheng <55902789+jzheng100823@users.noreply.github.com>
2021-12-08 19:23:01 -0500

    Update SPl language 1.py

--- SPl language 1.py
+++ SPl language 1.py
@@ -1,10 +1,10 @@
-def factorial(n):
+def factorial(n): #creates the factorial function
       
-    if n == 0:
+    if n == 0:   #this is the base case 
         return 1
      
-    return n * factorial(n-1)
+    return n * factorial(n-1)  # this is what actually calculates the factorial 
   
-num = int(input("Enter your value: "));
-print("Factorial of", num, "is",
-factorial(num))
+num = int(input("Enter your value: ")); # this gets the user input 
+print("Factorial of", num, "is", #this prints out the result to the user
+factorial(num)) # this is what actually calls the factorial function
